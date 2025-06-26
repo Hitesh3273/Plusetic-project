d92ad9fd7831d52d461c0f911b04f7ffe0ee57aa
Hitesh Bari <hiteshbari183@gmail.com>
2025-06-26 18:47:15 +0530

    Sign In Page add

--- scripts/header.js
+++ scripts/header.js
@@ -3,3 +3,8 @@ function show(){
       document.getElementById('NAVLIST').classList.toggle('hidden');
 }
  
+
+   function goToSignup() 
+    {
+     window.location.href = "../Sections/signIn.html"; // Change this to your actual signup URL
+    }
